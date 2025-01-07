f3c739cb5fb9e30eac0dc29d9bbc36c7c3351879
Akbar <babaevakbars@gmail.com>
2025-01-07 22:44:48 +0500

    Задание 2

--- Program.cs
+++ Program.cs
@@ -50,15 +50,64 @@ namespace MyLesson
                     }
                 }
             }
+        } 
+
+        static int[,] FullArray(int size1,int size2, int rand1, int rand2)
+        {
+            Random rand = new Random();
+            int[,] mac = new int [size1, size2];
+            int rowsMac = mac.GetUpperBound(0)+1;
+            int columsMac = mac.Length / rowsMac;
+            foreach (var i in mac)
+            {
+                for(int j = 0; j < rowsMac; j++)
+                {
+                    for (int k = 0; k < columsMac; k++)
+                    {
+                        int randomNum = rand.Next(rand1, rand2);
+                        mac[j, k] = randomNum;
+                    }
+                }
+            }
+            return mac;
+        }
+        static void SumArrayPositive(int[,] array)
+        {
+            int sum = 0;
+            foreach(int i in array)
+            {
+                if(i >= 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine($"Сумма положительных чисел равен: {sum}");
+
+        }
+
+        static void SumArrayNegative(int[,] array)
+        {
+            int sum = 0;
+            foreach(int i in array)
+            {
+                if(i < 0)
+                {
+                    sum -= i;
+                }
+            }
+            Console.WriteLine($"Сумма отрицательных чисел равен: -{sum}");
+
         }
         static void Main()
         {
-            // Задание 1. Определить число на полиндром
-            int[] array = {121, 234, 1221, 456, 989};
-            NumPalindrome(array);
-
+            // // Задание 1. Определить число на полиндром
+            // int[] array = {121, 234, 1221, 456, 989};
+            // NumPalindrome(array);
             
-            
+            // Задание 2. Сумма всех положительных и отрицательных чисел во многомнрном массиве.
+            int[,] a = FullArray(3, 4, -100, 100);            
+            SumArrayPositive(a);
+            SumArrayNegative(a);
         }
     }
 }
--- obj/Debug/net8.0/Lesson5.AssemblyInfo.cs
+++ obj/Debug/net8.0/Lesson5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd3a0afa651b7e618b95f1178eec579ffc0434f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1657a28da981e284eb8dfc717fc3335e5d318c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
